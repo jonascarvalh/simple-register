f2b3be7dbbeb67d945674898b5d2f8cddb4337b6
jonascarvalh <olaffic@gmail.com>
2023-11-28 11:21:52 -0300

    fix: kawargs to kwargs in form

--- enrollment/forms/register_form.py
+++ enrollment/forms/register_form.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 
 class RegisterForm(forms.ModelForm):
-    def __init__(self, *args, ***kawargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     # username
