e00456363376fed1c20659f09d57e331d68eafed
jonascarvalh <olaffic@gmail.com>
2024-01-05 13:22:56 -0300

    add: model Registration

--- enrollment/models.py
+++ enrollment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Registration(models.Model):
+    choice_type = (
+        ('S', 'Student'),
+        ('E', 'Employee'),
+        ('Ex', 'Extern'),
+    )
+    public_type = models.CharField(max_length=2, choices=choice_type)
+    registration = models.CharField(max_length=11)
+    
